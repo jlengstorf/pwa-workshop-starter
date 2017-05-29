2eedf0fa6e12c44e6704c81639a720fde6bde2cf
Jason Lengstorf <jlengstorf@users.noreply.github.com>
2017-05-29 20:14:37 +0200

    chore: update build script to (hopefully) work on Windows (#2)
    It appears that single quotes with globs cause Windows problems,
    as well as using `del` vs `del-cli`. This fix _should_ address the
    Windows build issues.
    
    It also moves off Yarn, which was a stumbling block for attendees.
    
    close #1

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-import { lazyLoadImages } from 'responsive-lazyload';
+import responsiveLazyload from 'responsive-lazyload';
 import lightbox from './lib/lightbox';
 import tabs from './lib/tabs';
 
 // For some reason, an IIFE doesn’t work here (or Babel breaks it).
 setTimeout(() => {
-  const loadVisibleImages = lazyLoadImages();
+  const loadVisibleImages = responsiveLazyload();
 
   lightbox();
   tabs({
